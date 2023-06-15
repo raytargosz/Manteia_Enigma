f4eec3bce00ae7c049295ecf2ea34c9627b38c6b
Ray Targosz <78501459+raytargosz@users.noreply.github.com>
2023-06-15 14:48:37 -0400

    working on the sword controller

--- Assets/Scripts/Player/SwordController.cs
+++ Assets/Scripts/Player/SwordController.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
 using System.Collections;
 
 public class SwordController : MonoBehaviour
 {
-    // Reference to the player controller
+    [Header("Player Controller")]
+    [Tooltip("Reference to the player controller")]
     public PlayerController playerController;
 
-    // Define some properties of the sword
+    [Header("Sword Properties")]
+    [Tooltip("Speed of the sword swing")]
     public float swingSpeed = 5f;
+    [Range(0, 10)]
+    [Tooltip("Speed of the sword jab")]
     public float jabSpeed = 5f;
-    public float swingAngle = 60f;  // Angle by which the sword swings
-    public float jabDistance = 0.5f;  // Distance by which the sword jabs forward
+    [Tooltip("Angle by which the sword swings")]
+    public float swingAngle = 60f;
+    [Tooltip("Distance by which the sword jabs forward")]
+    public float jabDistance = 0.5f;
+
+    [Header("Sword Bobbing")]
+    [Tooltip("Speed of bobbing while Idle")]
+    public float idleBobSpeed = 0.05f;
+    [Tooltip("Speed of bobbing while Walking")]
+    public float walkBobSpeed = 0.1f;
+    [Tooltip("Speed of bobbing while Running")]
+    public float runBobSpeed = 0.2f;
+    [Tooltip("Speed of bobbing while Jumping")]
+    public float jumpBobSpeed = 0.3f;
+    [Tooltip("Amount of bobbing (up and down movement)")]
+    [Range(0, 1)]
+    public float bobAmount = 0.05f;
+
+    [Header("Attack Settings")]
+    [Tooltip("Cool down time between each attack")]
+    public float attackCooldown = 1.0f;
+    private float lastAttackTime = 0f;
+
+    [Header("Animation Curves")]
+    [Tooltip("Animation curve for the sword swing")]
+    public AnimationCurve swingCurve;
+    [Tooltip("Animation curve for the sword jab")]
+    public AnimationCurve jabCurve;
+    [Tooltip("Animation curve for the sword bob")]
+    public AnimationCurve bobCurve;
+
+    [Header("Events")]
+    public UnityEvent OnSwingComplete;
+    public UnityEvent OnJabComplete;
 
     // Store the initial position and rotation of the sword for resetting after an action
     private Vector3 initialPosition;
@@ -34,28 +72,38 @@ public class SwordController : MonoBehaviour
 
         if (playerState == PlayerController.PlayerState.Idle && !isSwinging && !isJabbing)
         {
-            // Here we could implement some minor bobbing to match the camera's bobbing
-            // You would need to implement a similar logic as your camera's bobbing
-            // You might also need to expose some variables from your PlayerController for this to work properly
+            Bob(idleBobSpeed);
         }
         else if (playerState == PlayerController.PlayerState.Walking && !isSwinging && !isJabbing)
         {
-            // Adjust the sword's position and rotation to match the walking animation
+            Bob(walkBobSpeed);
         }
-        // Implement similar blocks for Running, Jumping, and Boosting
+        else if (playerState == PlayerController.PlayerState.Running && !isSwinging && !isJabbing)
+        {
+            Bob(runBobSpeed);
+        }
+        else if (playerState == PlayerController.PlayerState.Jumping && !isSwinging && !isJabbing)
+        {
+            Bob(jumpBobSpeed);
+        }
 
-        // Handle sword actions
-        if (Input.GetMouseButtonDown(0) && !isSwinging && !isJabbing) // Left click
+        if (Time.time > lastAttackTime + attackCooldown)
         {
-            StartCoroutine(SwingLeft());
-        }
-        else if (Input.GetMouseButtonDown(1) && !isSwinging && !isJabbing) // Right click
-        {
-            StartCoroutine(SwingRight());
-        }
-        else if (Input.mouseScrollDelta.y > 0 && !isSwinging && !isJabbing) // Mouse scroll up
-        {
-            StartCoroutine(Jab());
+            if (Input.GetMouseButtonDown(0) && !isSwinging && !isJabbing) // Left click
+            {
+                StartCoroutine(SwingLeft());
+                lastAttackTime = Time.time;
+            }
+            else if (Input.GetMouseButtonDown(1) && !isSwinging && !isJabbing) // Right click
+            {
+                StartCoroutine(SwingRight());
+                lastAttackTime = Time.time;
+            }
+            else if (Input.mouseScrollDelta.y > 0 && !isSwinging && !isJabbing) // Mouse scroll up
+            {
+                StartCoroutine(Jab());
+                lastAttackTime = Time.time;
+            }
         }
     }
 
@@ -68,15 +116,15 @@ public class SwordController : MonoBehaviour
 
         for (float t = 0; t <= 1; t += Time.deltaTime / swingSpeed)
         {
-            // Swing the sword to the left
-            transform.localRotation = Quaternion.Lerp(startRotation, endRotation, t);
+            // Swing the sword to the left using the animation curve
+            transform.localRotation = Quaternion.Slerp(startRotation, endRotation, swingCurve.Evaluate(t));
             yield return null;
         }
 
         // Swing back to initial position
         for (float t = 0; t <= 1; t += Time.deltaTime / swingSpeed)
         {
-            transform.localRotation = Quaternion.Lerp(endRotation, startRotation, t);
+            transform.localRotation = Quaternion.Slerp(endRotation, startRotation, swingCurve.Evaluate(t));
             yield return null;
         }
 
@@ -92,15 +140,15 @@ public class SwordController : MonoBehaviour
 
         for (float t = 0; t <= 1; t += Time.deltaTime / swingSpeed)
         {
-            // Swing the sword to the right
-            transform.localRotation = Quaternion.Lerp(startRotation, endRotation, t);
+            // Swing the sword to the right using the animation curve
+            transform.localRotation = Quaternion.Slerp(startRotation, endRotation, swingCurve.Evaluate(t));
             yield return null;
         }
 
         // Swing back to initial position
         for (float t = 0; t <= 1; t += Time.deltaTime / swingSpeed)
         {
-            transform.localRotation = Quaternion.Lerp(endRotation, startRotation, t);
+            transform.localRotation = Quaternion.Slerp(endRotation, startRotation, swingCurve.Evaluate(t));
             yield return null;
         }
 
@@ -116,18 +164,26 @@ public class SwordController : MonoBehaviour
 
         for (float t = 0; t <= 1; t += Time.deltaTime / jabSpeed)
         {
-            // Jab the sword forward
-            transform.localPosition = Vector3.Lerp(startPosition, endPosition, t);
+            // Jab the sword forward using the animation curve
+            transform.localPosition = Vector3.Lerp(startPosition, endPosition, jabCurve.Evaluate(t));
             yield return null;
         }
 
         // Move back to initial position
         for (float t = 0; t <= 1; t += Time.deltaTime / jabSpeed)
         {
-            transform.localPosition = Vector3.Lerp(endPosition, startPosition, t);
+            transform.localPosition = Vector3.Lerp(endPosition, startPosition, jabCurve.Evaluate(t));
             yield return null;
         }
 
         isJabbing = false;
     }
+
+    private void Bob(float bobSpeed)
+    {
+        float newY = initialPosition.y + Mathf.Sin(Time.time * bobSpeed) * bobAmount;
+        transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+    }
+
+    // Swing and Jab methods remain the same as the previous version, but you may want to modify them to use the animation curves.
 }
