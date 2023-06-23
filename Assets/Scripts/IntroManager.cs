9f6a0b7bcb9b187ee599ba9102172413fc01eaa0
Ray Targosz <78501459+raytargosz@users.noreply.github.com>
2023-06-23 12:52:10 -0400

    added custom fonts, added intro text.

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; // Namespace for TextMesh Pro
 using System.Collections;
 
 public class IntroManager : MonoBehaviour
 {
     public Image overlayImage; // Image to fade in/out
+    public TMP_Text introText; // TMP text element for intro text
+    public float textFadeOutDuration = 2f; // Time it takes for the text to fade out
     public float fadeDuration = 2f; // Time it takes for the overlay to fade
     public float playerControlDelay = 0.25f; // Delay before player can move after fade
     public AudioClip introSFX; // Intro sound effect
@@ -52,14 +55,25 @@ public class IntroManager : MonoBehaviour
 
         yield return new WaitForSeconds(playerControlDelay);
 
-        // Reset the player's position and rotation
-        playerTransform.position = initialPlayerPosition;
-
         // Assuming Y rotation is what we need to preserve, modify according to your needs
         playerTransform.rotation = Quaternion.Euler(playerTransform.eulerAngles.x, initialPlayerRotation.eulerAngles.y, playerTransform.eulerAngles.z);
 
         playerController.enabled = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+
+        StartCoroutine(FadeOutText());
+    }
+
+    // Coroutine to fade out the text
+    private IEnumerator FadeOutText()
+    {
+        float alpha = 1f;
+        while (alpha > 0f)
+        {
+            alpha -= Time.deltaTime / textFadeOutDuration;
+            introText.color = new Color(introText.color.r, introText.color.g, introText.color.b, alpha);
+            yield return null;
+        }
     }
 }
