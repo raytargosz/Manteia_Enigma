c89ff2cf0b5fd3d878ad520951a4c60eae72502a
Ray Targosz <78501459+raytargosz@users.noreply.github.com>
2023-06-26 15:54:35 -0400

    closer still with the room controllers. added 'room script'

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -10,34 +10,16 @@ public class Room : MonoBehaviour
     [Tooltip("Add the room's lights, props and other objects here.")]
     public List<GameObject> roomObjects = new List<GameObject>();
 
-    private RoomManager roomManager;
-
-    private void Start()
-    {
-        roomManager = FindObjectOfType<RoomManager>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == roomManager.player)
-        {
-            roomManager.RegisterRoom(this);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == roomManager.player)
-        {
-            roomManager.UnregisterRoom(this);
-        }
-    }
-
     public void SetActive(bool active)
     {
         foreach (var obj in roomObjects)
         {
             obj.SetActive(active);
         }
+    }
+
+    public bool IsPlayerInside(Vector3 playerPosition)
+    {
+        return roomArea.bounds.Contains(playerPosition);
     }
 }
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -10,7 +10,6 @@ public class RoomManager : MonoBehaviour
     public List<Room> rooms = new List<Room>();
 
     private Room currentRoom;
-    private List<Room> roomsPlayerIsInside = new List<Room>();
 
     private void Start()
     {
@@ -30,27 +29,21 @@ public class RoomManager : MonoBehaviour
 
     private void Update()
     {
-        // If player is not inside any room, then don't do anything
-        if (roomsPlayerIsInside.Count == 0)
-            return;
-
-        Room lastEnteredRoom = roomsPlayerIsInside[roomsPlayerIsInside.Count - 1];
-        // If the player has entered a new room, disable the current room and enable the new one.
-        if (currentRoom != lastEnteredRoom)
+        // Find the room the player is currently in.
+        foreach (var room in rooms)
         {
-            currentRoom.SetActive(false);
-            lastEnteredRoom.SetActive(true);
-            currentRoom = lastEnteredRoom;
+            if (room.IsPlayerInside(player.transform.position))
+            {
+                // If the player has entered a new room, disable the current room and enable the new one.
+                if (currentRoom != room)
+                {
+                    currentRoom.SetActive(false);
+                    room.SetActive(true);
+                    currentRoom = room;
+                }
+
+                break;
+            }
         }
-    }
-
-    public void RegisterRoom(Room room)
-    {
-        roomsPlayerIsInside.Add(room);
-    }
-
-    public void UnregisterRoom(Room room)
-    {
-        roomsPlayerIsInside.Remove(room);
     }
 }
