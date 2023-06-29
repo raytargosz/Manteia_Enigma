f3f0e4cf90716717aee32956571fc872a915a7ab
Ray Targosz <78501459+raytargosz@users.noreply.github.com>
2023-06-29 11:08:11 -0400

    Invitation scene good for now.

--- Assets/Scripts/Menus/ScrollTMP.cs
+++ Assets/Scripts/Menus/ScrollTMP.cs
@@ -5,29 +5,34 @@ using TMPro;
 
 public class ScrollTMP : MonoBehaviour, IScrollHandler
 {
+    [Tooltip("Reference to the TextMeshPro component")]
     public TMP_Text textComponent;
+
+    [Tooltip("Reference to the ScrollRect component")]
     public ScrollRect scrollRect;
 
     private void Start()
     {
-        // Ensure ScrollRect and TMP_Text are not null
+        // Check if ScrollRect and TMP_Text are assigned in the inspector
         if (textComponent == null || scrollRect == null)
         {
             Debug.LogError("ScrollRect or TMP_Text is not assigned in the inspector");
             return;
         }
 
-        // Enable vertical scrolling only
+        // Set the ScrollRect to only allow vertical scrolling
         scrollRect.horizontal = false;
         scrollRect.vertical = true;
     }
 
+    // This method is called when a scroll event is detected
     public void OnScroll(PointerEventData eventData)
     {
-        // Get the scroll delta
+        // Calculate the amount of scrolling in the y-axis
         float delta = eventData.scrollDelta.y;
 
-        // Change the normalized position of the ScrollRect based on the scroll delta
+        // Adjust the vertical normalized position of the ScrollRect based on the scroll delta
+        // Normalized position is a value between 0 and 1 representing the position of the viewport in the content
         scrollRect.verticalNormalizedPosition += delta * Time.deltaTime;
     }
 }
